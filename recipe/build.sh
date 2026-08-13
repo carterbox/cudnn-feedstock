6e0aa663df6ec5250c342fb16904182c698578e6
Daniel Ching <dching@nvidia.com>
2026-08-13 11:24:01 -0500

    BUG: Use correct glob for linux libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,4 +9,4 @@ cp -vp include/cudnn*.h $PREFIX/include/
 mkdir -p $PREFIX/lib
 cp -vpP lib/libcudnn*.so* $PREFIX/lib/
 
-python ${RECIPE_DIR}/check_cuda_arch.py ${PREFIX}/libcudnn*.so*
+python ${RECIPE_DIR}/check_cuda_arch.py ${PREFIX}/lib/libcudnn*.so
